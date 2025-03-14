a56d46b42aa3fdec1ad783800521d5e411cee4fd
SAICSP <chennasaicsp@gmail.com>
2025-03-14 22:14:16 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.set('trust proxy', 1);
 
 // Database connection
